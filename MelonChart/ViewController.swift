39d97b4177546fb45afc94e3de4574d2506a5f78
KangJungu <KangJungu@JHJG.com>
2016-04-10 16:28:05 +0900

    Initial Commit

---
+++ MelonChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MelonChart
+//
+//  Created by JHJG on 2016. 4. 10..
+//  Copyright © 2016년 KangJungu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
